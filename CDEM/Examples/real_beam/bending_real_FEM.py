932c88f8e1d956b3d1ca15a7867db471d2cb2c5f
Vancikv <vla.vancik@gmail.com>
2016-04-25 08:51:50 +0200

    synchro

--- CDEM/Examples/real_beam/bending_real_FEM.py
+++ CDEM/Examples/real_beam/bending_real_FEM.py
@@ -8,11 +8,12 @@ Created on 10. 11. 2015
 from CDEM import *
 import numpy as np
 
-dom = get_2D_quadrangle_domain(ni=3, nj=20, lx=2., ly=0.2, E=25000000000., nu=0.2, density=2500.,
+dom = get_2D_quadrangle_domain(ni=8, nj=96, lx=2., ly=0.2, E=25000000000., nu=0.2, density=2500.,
                              thickness=0.2, stiffdef='isoparametric',
-                             spring_stiffness_factor=10., domtype='CDEMStatic',
-                             load=[((0.99, 1.01), (0.19, 0.21), [0., -200000.])],
-                             supports=[((-0.01, 0.01), (-0.01, 0.01), [1, 1]), ((1.99, 2.01), (-0.01, 0.01), [0, 1])])
+                             spring_stiffness_factor=10.0, domtype='CDEMStatic',
+                             load=[((0.99999, 1.00001), (0.19999, 0.20001), [0., -200000.])],
+                             supports=[((-0.00001, 0.00001), (-0.00001, 0.00001), [1, 1]), ((1.99999, 2.00001), (-0.00001, 0.00001), [0, 1])])
 
+dom.save_to_text_file("c:/Users/Kapsak/Dropbox/CDEM/Cpp_data/3pb_binary/3pb.txt")
 dom.solve()
 dom.plot(magnitude=10.)
